7938202b14c1159d0bec9731992611a2dd70e39d
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-10-12 12:00:13 -0500

    avance de ui, visitante y admin

---
+++ js/app.js
@@ -0,0 +1,84 @@
+const btnVisitor = document.getElementById('btnVisitor');
+const main = document.querySelector('.main');
+const register = document.querySelector('.register');
+const adminPage = document.querySelector('.admin');
+const returnI = document.querySelector('#returnInit');
+const btnAdmin = document.querySelector('#btnAdmin');
+const returnI2 = document.querySelector('#returnInit2');
+
+btnVisitor.addEventListener('click', () => {
+  main.classList.add('back');
+  register.classList.remove("back");
+});
+
+btnAdmin.addEventListener('click', ()=>{
+	main.classList.add('back');
+  adminPage.classList.remove("back");
+});
+
+returnI.addEventListener('click', () => {
+  main.classList.remove('back');
+  register.classList.add("back");
+});
+
+returnI2.addEventListener('click', () => {
+  main.classList.remove('back');
+  adminPage.classList.add("back");
+});
+
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+  $('.datepicker').datepicker();
+	$('.timepicker').timepicker();
+	$('select').formSelect();
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+	var video = document.getElementById('video');
+	var localStream = null;
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var errBack = function(e) {
+		console.log('Opps.. no se puede utilizar la cámara', e);
+	};
+ 
+	// Solicitar acceso a la cámara
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		});
+	}
+	else if(navigator.getUserMedia) { // Standard
+		navigator.getUserMedia({ video: true }, function(stream) {
+			video.src = stream;
+			video.play();
+			localStream = stream;
+		}, errBack);
+	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+		navigator.webkitGetUserMedia({ video: true }, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+			localStream = stream;
+		}, errBack);
+	} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+		navigator.mozGetUserMedia({ video: true }, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+			localStream = stream;
+		}, errBack);
+	}
+ 
+	document.getElementById('tomar').addEventListener('click', function() {
+    context.drawImage(video, 0, 0, 480, 360);
+    const c = document.getElementById('contenido');
+   	c.innerText = canvas.toDataURL();
+  });
+  // esto funcionará solamente si tienen una etiqueta button o similar con el id "detener".
+document.getElementById('detener').addEventListener('click', function() {
+	video.src = '';
+	video.pause();
+	localStream.getVideoTracks()[0].stop();
+});
+}, false);
---
+++ js/data.js
@@ -0,0 +1,95 @@
+const dataVisit = document.querySelector('#test4');
+const form = document.querySelector('#data-form');
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB1Kvoqn3MC9etQ7hk1ZWJnbisXfpSHxVw",
+  authDomain: "youwork-01.firebaseapp.com",
+  databaseURL: "https://youwork-01.firebaseio.com",
+  projectId: "youwork-01",
+  storageBucket: "youwork-01.appspot.com",
+  messagingSenderId: "982859617948"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+db.settings({
+  timestampsInSnapshots :true
+});
+
+//creo los elementos q se van a mostrar por cada doc
+const infoVisit = (doc)=>{
+  const box = document.createElement('div');
+  const name = document.createElement('p');
+  const date = document.createElement('p');
+  const dni = document.createElement('p');
+  const email = document.createElement('p');
+  const hour = document.createElement('p');
+  const cross = document.createElement('div');
+
+
+  box.setAttribute('id', doc.id);
+  box.setAttribute('class', 'infoVisit');
+  cross.setAttribute('class', 'center-align btn-delete');
+  box.setAttribute('style', 'border: 1px solid black;');
+  name.textContent = doc.data().name;
+  date.textContent = doc.data().date;
+  dni.textContent = doc.data().dni;
+  email.textContent = doc.data().email;
+  hour.textContent = doc.data().hour;
+  cross.textContent = 'Eliminar';
+
+  box.appendChild(name);
+  box.appendChild(date);
+  box.appendChild(dni);
+  box.appendChild(email);
+  box.appendChild(hour);
+  box.appendChild(cross);
+
+  dataVisit.appendChild(box);
+
+  //eliminar visita
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('id');
+    db.collection('visitors').doc(id).delete();
+  })
+}
+
+//enlazamos con la db y obtenemos los datos cada uno un objeto
+// db.collection('visitors').get()
+//   .then((snapshot)=>{
+//     snapshot.docs.forEach(doc => {
+//       infoVisit(doc);
+//     })
+//   });
+//guardando la data del formulario
+
+form.addEventListener('submit', (e)=>{
+  console.log("guardado");
+  e.preventDefault();
+  db.collection('visitors').add({
+    name: form.name.value,
+    date: form.date.value,
+    dni: form.dni.value,
+    email: form.email.value,
+    hour: form.hour.value,
+  });
+  form.name.value="";
+  form.date.value="";
+  form.dni.value=null;
+  form.email.value="";
+  form.hour.value="";
+  M.toast({html: 'Notificación enviada'})
+})
+
+//eventos en tiempo real
+db.collection('visitors').orderBy('name').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change =>{
+    if(change.type === 'added'){
+      infoVisit(change.doc);
+    } else if(change.type === 'removed'){
+      let visit = dataVisit.querySelector(`#${change.doc.id}`);
+      dataVisit.removeChild(visit);
+    }
+  })
+})
